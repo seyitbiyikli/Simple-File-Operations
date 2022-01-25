991aa1e42cf944dff7be5e9d7b910976bcc97cb5
Seyit Bıyıklı <75814226+seyitbiyikli@users.noreply.github.com>
2022-01-26 00:52:29 +0300

    Add files via upload

---
+++ Simple-File-Operations/dosya_kullanim_odevi/Form1.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace dosya_kullanim_odevi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.adTxt = new System.Windows.Forms.TextBox();
+            this.soyadTxt = new System.Windows.Forms.TextBox();
+            this.noTxt = new System.Windows.Forms.TextBox();
+            this.bolumTxt = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dyeriTxt = new System.Windows.Forms.TextBox();
+            this.yasTxt = new System.Windows.Forms.TextBox();
+            this.telnoTxt = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            this.aramaTxt = new System.Windows.Forms.TextBox();
+            this.aramaCmb = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kayıt Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kayıtları Listele";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(226, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Kayıt Düzenle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(326, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Çıkış";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Soyadı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Numarası:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Bölümü:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Cinsiyeti:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Doğum Yeri:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 237);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Yaşı:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 271);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Tel. Numara:";
+            // 
+            // adTxt
+            // 
+            this.adTxt.Location = new System.Drawing.Point(115, 51);
+            this.adTxt.Name = "adTxt";
+            this.adTxt.Size = new System.Drawing.Size(100, 20);
+            this.adTxt.TabIndex = 13;
+            // 
+            // soyadTxt
+            // 
+            this.soyadTxt.Location = new System.Drawing.Point(115, 78);
+            this.soyadTxt.Name = "soyadTxt";
+            this.soyadTxt.Size = new System.Drawing.Size(100, 20);
+            this.soyadTxt.TabIndex = 14;
+            // 
+            // noTxt
+            // 
+            this.noTxt.Location = new System.Drawing.Point(115, 109);
+            this.noTxt.Name = "noTxt";
+            this.noTxt.Size = new System.Drawing.Size(100, 20);
+            this.noTxt.TabIndex = 15;
+            // 
+            // bolumTxt
+            // 
+            this.bolumTxt.Location = new System.Drawing.Point(115, 140);
+            this.bolumTxt.Name = "bolumTxt";
+            this.bolumTxt.Size = new System.Drawing.Size(100, 20);
+            this.bolumTxt.TabIndex = 16;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Erkek",
+            "Kadın"});
+            this.comboBox1.Location = new System.Drawing.Point(115, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // dyeriTxt
+            // 
+            this.dyeriTxt.Location = new System.Drawing.Point(115, 203);
+            this.dyeriTxt.Name = "dyeriTxt";
+            this.dyeriTxt.Size = new System.Drawing.Size(100, 20);
+            this.dyeriTxt.TabIndex = 18;
+            // 
+            // yasTxt
+            // 
+            this.yasTxt.Location = new System.Drawing.Point(115, 234);
+            this.yasTxt.Name = "yasTxt";
+            this.yasTxt.Size = new System.Drawing.Size(100, 20);
+            this.yasTxt.TabIndex = 19;
+            // 
+            // telnoTxt
+            // 
+            this.telnoTxt.Location = new System.Drawing.Point(115, 268);
+            this.telnoTxt.Name = "telnoTxt";
+            this.telnoTxt.Size = new System.Drawing.Size(100, 20);
+            this.telnoTxt.TabIndex = 20;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(226, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(639, 292);
+            this.dataGridView1.TabIndex = 22;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(432, 358);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Kayıt Ara";
+            // 
+            // aramaTxt
+            // 
+            this.aramaTxt.Location = new System.Drawing.Point(487, 355);
+            this.aramaTxt.Name = "aramaTxt";
+            this.aramaTxt.Size = new System.Drawing.Size(121, 20);
+            this.aramaTxt.TabIndex = 24;
+            this.aramaTxt.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // aramaCmb
+            // 
+            this.aramaCmb.FormattingEnabled = true;
+            this.aramaCmb.Items.AddRange(new object[] {
+            "Ada göre arama",
+            "Numaraya göre arama"});
+            this.aramaCmb.Location = new System.Drawing.Point(295, 354);
+            this.aramaCmb.Name = "aramaCmb";
+            this.aramaCmb.Size = new System.Drawing.Size(121, 21);
+            this.aramaCmb.TabIndex = 25;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(223, 358);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(66, 13);
+            this.label10.TabIndex = 26;
+            this.label10.Text = "Arama Kriteri";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(115, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "Text Temizle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(223, 39);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(319, 13);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "Düzenlemeyi liste üzerinden yapınız ve düzenle butonuna basınız!!!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 416);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.aramaCmb);
+            this.Controls.Add(this.aramaTxt);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.telnoTxt);
+            this.Controls.Add(this.yasTxt);
+            this.Controls.Add(this.dyeriTxt);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.bolumTxt);
+            this.Controls.Add(this.noTxt);
+            this.Controls.Add(this.soyadTxt);
+            this.Controls.Add(this.adTxt);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox adTxt;
+        private System.Windows.Forms.TextBox soyadTxt;
+        private System.Windows.Forms.TextBox noTxt;
+        private System.Windows.Forms.TextBox bolumTxt;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox dyeriTxt;
+        private System.Windows.Forms.TextBox yasTxt;
+        private System.Windows.Forms.TextBox telnoTxt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox aramaTxt;
+        private System.Windows.Forms.ComboBox aramaCmb;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Simple-File-Operations/dosya_kullanim_odevi/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace dosya_kullanim_odevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string dosyaYolu = @"C:\Users\User\Desktop\ogrenciler.dat";
+        DataTable dt = new DataTable();
+
+        void dosyaOlustur()
+        {
+            FileStream fs = File.Create(dosyaYolu);
+            fs.Close();
+        }
+
+        void kisiEkle(string ad, string soyad, string numara, string bolum, string cinsiyet, string dYeri, string yas, string telNo)
+        {
+            FileStream fs = new FileStream(dosyaYolu, FileMode.OpenOrCreate, FileAccess.Write);
+            fs.Close();
+            string text =ad + "/" + soyad + "/" + numara + "/" + bolum + "/" + cinsiyet + "/" + dYeri + "/" + yas + "/" + telNo;
+            File.AppendAllText(dosyaYolu, Environment.NewLine + text);
+            MessageBox.Show("Kişi eklenip,kayıtlar listelendi");
+        }
+        void listele()
+        {
+            string[] satir = File.ReadAllLines(dosyaYolu);
+            int a = dataGridView1.RowCount-1;
+            for (int i = a; i < satir.Length; i++)
+            {
+                string[] veri = satir[i].ToString().Split('/');
+                string[] satir2 = new string[veri.Length];
+                for (int j = 0; j < veri.Length; j++)
+                {
+                    satir2[j] = veri[j].Trim();
+                }
+                dt.Rows.Add(satir2);
+            }
+            
+        }
+
+        void guncelle()
+        {
+            TextWriter yazma = new StreamWriter(dosyaYolu);
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if (j == 7)
+                    {
+                        yazma.Write(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                    }
+                    else
+                    {
+                        yazma.Write(dataGridView1.Rows[i].Cells[j].Value.ToString() + "/");
+                    }
+                }
+                yazma.WriteLine("");
+            }
+            yazma.Close();
+        }
+        void txtTemizle()
+        {
+            adTxt.Text = "";
+            soyadTxt.Text = "";
+            noTxt.Text = "";
+            bolumTxt.Text = "";
+            comboBox1.Text = "";
+            dyeriTxt.Text = "";
+            yasTxt.Text = "";
+            telnoTxt.Text = "";
+        }
+        void txtDoldur()
+        {
+            adTxt.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            soyadTxt.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            noTxt.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            bolumTxt.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            comboBox1.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            dyeriTxt.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            yasTxt.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+            telnoTxt.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+        }
+        void dataDoldur()
+        {
+            dt.Columns.Add("Adı", typeof(string));
+            dt.Columns.Add("Soyadı", typeof(string));
+            dt.Columns.Add("Numarası", typeof(string));
+            dt.Columns.Add("Bölümü", typeof(string));
+            dt.Columns.Add("Cinsiyeti", typeof(string));
+            dt.Columns.Add("Doğum Yeri", typeof(string));
+            dt.Columns.Add("Yaşı", typeof(string));
+            dt.Columns.Add("Telefon Numara", typeof(string));
+            dataGridView1.DataSource = dt;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            if (File.Exists(dosyaYolu))
+            {
+            }
+            else
+            {
+                dosyaOlustur();
+            }
+            dataDoldur();
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            listele();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cins;
+            if (comboBox1.SelectedIndex == 0)
+            {
+                cins = "Erkek";
+            }
+            else
+                cins = "Kadın";
+            kisiEkle(adTxt.Text, soyadTxt.Text, noTxt.Text, bolumTxt.Text, cins, dyeriTxt.Text, yasTxt.Text, telnoTxt.Text);
+            listele();
+            txtTemizle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.CurrentRow.Selected = true;
+            txtDoldur();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.CurrentRow.Selected = true;
+            txtDoldur();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            DataView dv = dt.DefaultView;
+            if (aramaCmb.SelectedIndex == 0)
+            {
+                dv.RowFilter = "Adı LIKE '" + aramaTxt.Text + "%'";
+                dataGridView1.DataSource = dv;
+            }
+            else
+            {
+                dv.RowFilter = "Numarası LIKE '" + aramaTxt.Text + "%'";
+                dataGridView1.DataSource = dv;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            guncelle();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtTemizle();
+        }
+
+
+    }
+}
